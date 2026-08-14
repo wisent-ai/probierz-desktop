383469dcd52ab1e47a599a6d72c0fe12283ab9e3
lbartoszcze <lgb2127@columbia.edu>
2026-08-14 13:41:37 -0700

    feat: adopt unified Wisent sign-in

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .executable(name: "ProbierzDesktop", targets: ["ProbierzDesktop"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/wisent-ai/wisent-desktop-auth.git", revision: "1619d5f"),
+        .package(url: "https://github.com/wisent-ai/wisent-desktop-auth.git", revision: "29cbd0d"),
         .package(url: "https://github.com/wisent-ai/wisent-desktop-update.git", exact: "0.1.0"),
         .package(url: "https://github.com/wisent-ai/echo.git", exact: "0.1.2"),
         .package(url: "https://github.com/wisent-ai/wisent-components.git", revision: "528a955"),
