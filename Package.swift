cd2bbc82284c0ede14fda4f45a07fa2ecc8545c4
lbartoszcze <lgb2127@columbia.edu>
2026-08-15 09:27:22 -0700

    chore: consume exact login components

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .executable(name: "ProbierzDesktop", targets: ["ProbierzDesktop"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/wisent-ai/wisent-desktop-auth.git", revision: "9b1f8d0"),
+        .package(url: "https://github.com/wisent-ai/wisent-desktop-auth.git", revision: "1e2dde5"),
         .package(url: "https://github.com/wisent-ai/wisent-desktop-update.git", exact: "0.1.0"),
         .package(url: "https://github.com/wisent-ai/echo.git", exact: "0.1.2"),
         .package(url: "https://github.com/wisent-ai/wisent-components.git", revision: "528a955"),
